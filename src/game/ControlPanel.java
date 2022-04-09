d375e372d79847b5fb44431adf885b40933aa973
MohammedNahin <Mohammed.Ahmed.30@city.ac.uk>
2022-04-09 22:51:51 +0100

    Added Control Panel

---
+++ src/game/ControlPanel.java
@@ -0,0 +1,11 @@
+package game;
+
+import javax.swing.*;
+
+public class ControlPanel {
+    private JPanel mainPanel;
+    private JButton pauseButton;
+    private JButton resumeButton;
+    private JButton restartButton;
+    private Game game;
+}
