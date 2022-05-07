e7dc82472c5be3ee19452aa879acd8cffc066b45
MohammedNahin <Mohammed.Ahmed.30@city.ac.uk>
2022-05-07 17:54:33 +0100

    Comments on Lavafloor

--- src/game/LavaFloor.java
+++ src/game/LavaFloor.java
@@ -6,7 +6,7 @@ import city.cs.engine.Shape;
 import java.awt.*;
 
 public class LavaFloor extends StaticBody {
-
+//sets width and height of the lavafloor
     private static final Shape floorShape = new BoxShape(4,0.5f);
 
     public LavaFloor(World w) {
